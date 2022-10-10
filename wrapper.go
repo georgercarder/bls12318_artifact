633cb65be57d922b906296261f565abfe69d17db
George Carder <georgercarder@gmail.com>
2022-10-10 16:10:39 -0700

    update wrapper

--- wrapper.go
+++ wrapper.go
@@ -2,28 +2,45 @@ package main
 
 import (
     "C"
+    "unsafe"
 
     "github.com/ethereum/go-ethereum/common"
     "github.com/ethereum/go-ethereum/core/vm"
 )
 
 //export RunContractAt
-func RunContractAt(addr uint64, input []byte, suppliedGas uint64) (ret []byte, errString string, gasUsed uint64) {
+func RunContractAt(addr uint64, input []byte, suppliedGas uint64) (gasUsed uint64, ret unsafe.Pointer, retLen uint64, errString *C.char, errLen uint64) {
+
+    goString := ""
 
     _addr := common.BytesToAddress([]byte{byte(addr)}) 
     precompile := vm.PrecompiledContractsBLS[_addr]
     if precompile == nil { // addr range [10, 18], see go-ethereum/core/vm 
-        return ret, "address not BLS", 0
+        goString = "address not BLS"
+        errLen = uint64(len(goString))
+        errString = C.CString(goString)
+        gasUsed = 0
+        return
     }
-    gasCost := precompile.RequiredGas(input)
-    if suppliedGas < gasCost {
-        return ret, "insufficient gas", 0
+    gasUsed = precompile.RequiredGas(input)
+    if suppliedGas < gasUsed {
+        goString := "insufficient gas"
+        errLen = uint64(len(goString))
+        errString = C.CString(goString)
+        gasUsed = 0
+        return 
     }
     output, err := precompile.Run(input)
     if err != nil {
-        errString = err.Error()
+        goString := err.Error()
+        errLen = uint64(len(goString))
+        errString = C.CString(goString)
     }
-    return output, errString, gasCost
+    if output != nil {
+        ret = C.CBytes(output)
+        retLen = uint64(len(output))
+    }
+    return
 }
 
 func main() {}
